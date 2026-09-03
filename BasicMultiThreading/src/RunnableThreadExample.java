42ba754558e10dccb9d1e394fb76c79ef64bf3a9
Manabendu-ai <rikugithub@gmail.com>
2026-09-03 13:59:32 +0530

    ThreadOne class

---
+++ BasicMultiThreading/src/RunnableThreadExample.java
@@ -0,0 +1,16 @@
+
+public class RunnableThreadExample {
+
+    /**
+     Implementing basic multithreading using Runnable interface.
+     */
+}
+
+class ThreadOne implements Runnable{
+    @Override
+    public void run(){
+        for(int i=0; i<5; i++){
+            System.out.println("Thread One : "+ i);
+        }
+    }
+}
