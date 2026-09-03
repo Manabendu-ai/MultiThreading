650de18009c3a050da9114c9c887a92a07f3fb9f
Manabendu-ai <rikugithub@gmail.com>
2026-09-03 14:00:04 +0530

    ThreadTwo class

--- BasicMultiThreading/src/RunnableThreadExample.java
+++ BasicMultiThreading/src/RunnableThreadExample.java
@@ -14,3 +14,12 @@ class ThreadOne implements Runnable{
         }
     }
 }
+
+class ThreadTwo implements Runnable{
+    @Override
+    public void run(){
+        for(int i=0; i<5; i++){
+            System.out.println("Thread Two : "+ i);
+        }
+    }
+}
