22a9f3b1e7ae7b9596b79ac87162d88b655e13ac
Manabendu-ai <rikugithub@gmail.com>
2026-09-03 15:36:57 +0530

    executing daemon thread and user thread.

--- BasicMultiThreading/src/DaemonUserThreadDemo.java
+++ BasicMultiThreading/src/DaemonUserThreadDemo.java
@@ -1,4 +1,21 @@
 public class DaemonUserThreadDemo {
+    public static void main(String[] args) {
+        Thread bgThread = new Thread(new DaemonHelper());
+        Thread userThread = new Thread(new UserThread());
+
+        bgThread.setDaemon(true);
+        bgThread.start();
+        userThread.start();
+        /**
+
+         Here is the flow of what exactly happened:
+         The userThread has a sleep time of 5 sec. So it was not assigned to any of the cpu.
+         The Daemon thread kept running in the background for the first 5 second, then when userThread resumed and completed
+         it execution and got terminated, The daemon thread also got terminated instantly because there was no other thread left
+         for execution.
+
+         */
+    }
 }
 
 class DaemonHelper implements Runnable{
