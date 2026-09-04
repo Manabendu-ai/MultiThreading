450b66280e4ba6b7f86e772237f425bdd31e6e25
Manabendu-ai <rikugithub@gmail.com>
2026-09-04 15:03:22 +0530

    executing ScheduledExecutorDemo

--- ExecutorServiceJava/src/ScheduledExecutorDemo.java
+++ ExecutorServiceJava/src/ScheduledExecutorDemo.java
@@ -1,4 +1,20 @@
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class ScheduledExecutorDemo {
+    public static void main(String[] args) {
+        ScheduledExecutorService service = new ScheduledThreadPoolExecutor(1);
+        service.scheduleAtFixedRate(new ProbeTask(), 1000,2000, TimeUnit.MILLISECONDS);
+
+        try{
+            if(!service.awaitTermination(10000, TimeUnit.MILLISECONDS)){
+                service.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            service.shutdownNow();
+        }
+    }
 }
 
 class ProbeTask implements Runnable{
