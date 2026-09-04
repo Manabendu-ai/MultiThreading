8d3007a7406e79f248f3cb9394bdf4a5e66461c3
Manabendu-ai <rikugithub@gmail.com>
2026-09-04 13:04:37 +0530

    calling producer consumer

--- ThreadSynchronisation/src/ProducerConsumer.java
+++ ThreadSynchronisation/src/ProducerConsumer.java
@@ -3,28 +3,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
-
- The Producer-Consumer problem is a synchronization scenario where one or more producer threads generate data and put into
- shared buffer, while one or more consumer threads retrieve and process the data from the buffer concurrently.
-
-
+ * The Producer-Consumer problem is a synchronization scenario where one or more producer threads generate data and put into
+ * shared buffer, while one or more consumer threads retrieve and process the data from the buffer concurrently.
  */
-class Worker{
+class Worker {
     private int sequence = 0;
     private final int TOP = 5;
     private final int BOTTOM = 0;
     private final LinkedList<Integer> container = new LinkedList<>();
-    private final Object LOCK =  new Object();
+    private final Object LOCK = new Object();
 
 
     public void produce() throws InterruptedException {
-        synchronized (LOCK){
-            while (true){
-                if(container.size()==TOP){
+        synchronized (LOCK) {
+            while (true) {
+                if (container.size() == TOP) {
                     System.out.println("Container is full! Waiting for the consumer to consume it.");
                     LOCK.wait();
-                } else{
-                    System.out.println(sequence+" Added to the container!");
+                } else {
+                    System.out.println(sequence + " Added to the container!");
                     container.add(sequence++);
                     LOCK.notify(); // this doesn't come into the picture immediately
                 }
@@ -34,22 +31,41 @@ class Worker{
 
     }
 
-    public void consume(){
-        synchronized (LOCK){
-            while(true){
-                if(container.size()==BOTTOM){
+    public void consume() throws InterruptedException {
+        synchronized (LOCK) {
+            while (true) {
+                if (container.size() == BOTTOM) {
                     System.out.println("Container is empty! Waiting for the producer to produce.");
-                }else{
-                    System.out.println(container.removeFirst()+" Consumed by the container");
+                } else {
+                    System.out.println(container.removeFirst() + " Consumed by the container");
                     LOCK.notify();
                 }
+                Thread.sleep(1000);
             }
         }
     }
 }
+
 public class ProducerConsumer {
 
     public static void main(String[] args) {
+        Worker worker = new Worker();
+        Thread producer = new Thread(
+                () -> {
+                    try {
+                        worker.produce();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
 
+        Thread consumer = new Thread(() -> {
+            try {
+                worker.produce();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
