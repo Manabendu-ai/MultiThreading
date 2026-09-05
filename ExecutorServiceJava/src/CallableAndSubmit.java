81729fb39629a364ec406cac0faecd71f3e9e40c
Manabendu-ai <rikugithub@gmail.com>
2026-09-05 12:51:28 +0530

    Difference between callable and runnable

---
+++ ExecutorServiceJava/src/CallableAndSubmit.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableAndSubmit {
+    public static void main(String[] args) {
+        /**
+         * Difference between callable and runnable.
+         * callable is similar to runnable but callable return a result and runnable doesn't
+         * callable return us an object of Future<T>.
+         * Future is a generic class where the results are obtained after a certain time in the future.
+         * In callable the exceptions are retained whereas in runnable the exceptions are not retained.
+         *
+         * submit() method: it is similar to execute() but it takes a callable object and return a future object.
+         */
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        List<Future<Integer>> res = new ArrayList<>();
+        for(int i = 1; i<15; i++){
+            int num = i;
+            Future<Integer> submit = service.submit(
+                    () -> num + 10
+            );
+            res.add(submit);
+        }
+
+        // getting the result from the future object
+        for(Future<Integer> num : res){
+
+        }
+    }
+}
