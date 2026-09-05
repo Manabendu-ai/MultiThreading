5b17ca0da5eae9e3d41336252112aafbb5353472
Manabendu-ai <rikugithub@gmail.com>
2026-09-05 13:30:17 +0530

    setting up the task.

--- ExecutorServiceJava/src/ThreadPoolExecutorDemo.java
+++ ExecutorServiceJava/src/ThreadPoolExecutorDemo.java
@@ -7,5 +7,21 @@ public class ThreadPoolExecutorDemo {
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
                 2, 5, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<>(5)
         );
+
+        for(int i =0; i<=5; i++){
+            int taskId = i;
+            executor.execute(
+                    ()-> {
+                        System.out.println("TaskID: " + taskId + " is performed by " + Thread.currentThread().getName());
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            );
+        }
+
+        executor.shutdown();
     }
 }
